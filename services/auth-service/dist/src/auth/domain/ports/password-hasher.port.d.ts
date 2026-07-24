02690b85ad31affd2e91fd2fd2b38c5fdc60acec
Manat47 <mmetthapriya147@gmail.com>
2026-07-24 14:39:06 +0700

    feat(auth ): delete dist

--- services/auth-service/dist/src/auth/domain/ports/password-hasher.port.d.ts
+++
@@ -1,5 +0,0 @@
-export declare const PASSWORD_HASHER: unique symbol;
-export interface PasswordHasher {
-    hash(password: string): Promise<string>;
-    compare(plainPassword: string, passwordHash: string): Promise<boolean>;
-}
--- services/auth-service/dist/src/auth/infrastructure/security/bcrypt-password-hasher.d.ts
+++
@@ -1,6 +0,0 @@
-import type { PasswordHasher } from '../../domain/ports/password-hasher.port';
-export declare class BcryptPasswordHasher implements PasswordHasher {
-    private readonly saltRounds;
-    hash(password: string): Promise<string>;
-    compare(plainPassword: string, passwordHash: string): Promise<boolean>;
-}
