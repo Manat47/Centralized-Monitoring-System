e7fff2761b9294a0bde758eb7a0cc66623ce6232
Manat47 <mmetthapriya147@gmail.com>
2026-07-27 13:09:23 +0700

    chore: stop tracking build artifacts

--- services/notification-service/dist/notification/infrastructure/providers/console-notification.sender.d.ts
+++
@@ -1,5 +0,0 @@
-import type { NotificationSender, SendNotificationInput } from '../../domain/ports/notification-sender.port';
-export declare class ConsoleNotificationSender implements NotificationSender {
-    private readonly logger;
-    send(input: SendNotificationInput): Promise<void>;
-}
